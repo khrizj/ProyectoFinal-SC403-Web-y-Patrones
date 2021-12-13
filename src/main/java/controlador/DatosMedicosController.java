9312088ac5a45258761cdc959496a39955dea1ef
khrizj <khrizj@gmail.com>
2021-12-13 05:36:39 -0600

    changes on odontologos

--- src/main/java/controlador/DatosMedicosController.java
+++ src/main/java/controlador/DatosMedicosController.java
@@ -3,7 +3,6 @@ package controlador;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
-import java.util.List;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import modelo.DatosMedicos;
@@ -18,6 +17,70 @@ import gestion.DatosMedicosGestion;
 public class DatosMedicosController extends DatosMedicos implements Serializable {
 
     public DatosMedicosController() {
+        super("","","",false,"","","");
     }
     
+     public String datosMedInserta() {
+
+        if (DatosMedicosGestion.datosInsert(this)) {
+            return "/index.xhtml";
+        } else {
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Error", "Posible Identificación Duplicada");
+            FacesContext.getCurrentInstance().addMessage("nuevoPacienteForm:Cedula", mensaje);
+            return "/Pacientes/nuevoIngreso.xhtml";
+        }
+    }
+
+    public String datosMedModifica() {
+
+        if (DatosMedicosGestion.datosUpdate(this)) {
+            return "/Pacientes/pacientes.xhtml";
+
+        } else {
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Error", "Posible Identificación Duplicada");
+            FacesContext.getCurrentInstance().addMessage("editaPacienteForm:identificacion", mensaje);
+            return "/Pacientes/pacientes.xhtml";
+        }
+
+    }
+
+    public String datosMedElimina() {
+
+        if (DatosMedicosGestion.datosDelete(this)) {
+            return "/Doctores/Odontologos.xhtml";
+        } else {
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Error", "Posible que el id no exista");
+            FacesContext.getCurrentInstance().addMessage("editaPacienteForm:identificacion", mensaje);
+            return "/Doctores/Odontologos.xhtml";
+        }
+
+    }
+
+    public String datosMedEdita(String cedPaciente) {
+
+        DatosMedicos datosMed = DatosMedicosGestion.getDato(cedPaciente);
+
+        if (datosMed != null) {
+
+            this.setAntecedentesPatologicosPersonales(datosMed.getAntecedentesPatologicosPersonales());
+            this.setAntecedentesPatologicosFamiliares(datosMed.getAntecedentesPatologicosFamiliares());
+            this.setAntecedentesQuirurgicos(datosMed.getAntecedentesQuirurgicos());
+            this.setAlergia(datosMed.isAlergia());
+            this.setAlergiaDetalle(datosMed.getAlergiaDetalle());
+            this.setMedicamentosConsumidos(datosMed.getMedicamentosConsumidos());
+            this.setCedulaPaciente(datosMed.getCedulaPaciente());
+            return "/Pacientes/pacientes.xhtml";
+        } else {
+
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Error", "Posiblemente el id no exista");
+            FacesContext.getCurrentInstance().addMessage("pacientesForm", mensaje);
+            return "/Pacientes/pacientes.xhtml";
+        }
+
+    }
+
 }
