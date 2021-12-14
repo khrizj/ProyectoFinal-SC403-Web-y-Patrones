6a24b31a94b05950337a7c38d08d38a63bff31b8
marychv <mariacv3199@gmail.com>
2021-12-13 22:52:28 -0600

    Update CitaController.java
    CRUD a cita controller

--- src/main/java/controlador/CitaController.java
+++ src/main/java/controlador/CitaController.java
@@ -13,12 +13,10 @@ import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import modelo.Cita;
 
-
 /**
  *
  * @author maric
  */
-
 @Named(value = "citaController")
 @SessionScoped
 public class CitaController extends Cita implements Serializable {
@@ -26,17 +24,62 @@ public class CitaController extends Cita implements Serializable {
     public CitaController() {
     }
 
-   
-    
-     public String inserta (){
-        
-        if (CitaGestion.citaInsert(this)){
+    public String inserta() {
+
+        if (CitaGestion.citaInsert(this)) {
             return "/Pacientes/pacientes.xhtml";
-        }else{
-            FacesMessage mensaje= new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                    "Error","Posible Identificación Duplicada");
+        } else {
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Error", "Posible Identificación Duplicada");
             FacesContext.getCurrentInstance().addMessage("editaEstudianteForm:identificacion", mensaje);
             return "pacientes.xhtml";
         }
+    }
+
+    public String citaModifica() {
+
+        if (CitaGestion.citaUpdate(this)) {
+            return "/Pacientes/pacientes.xhtml";
+
+        } else {
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Error", "Posible Identificación Duplicada");
+            FacesContext.getCurrentInstance().addMessage("editaPacienteForm:identificacion", mensaje);
+            return "/Pacientes/pacientes.xhtml";
+        }
+
+    }
+
+    public String citaElimina() {
+
+        if (CitaGestion.citaDelete(this)) {
+            return "/Doctores/Odontologos.xhtml";
+        } else {
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Error", "Posible que el id no exista");
+            FacesContext.getCurrentInstance().addMessage("editaPacienteForm:identificacion", mensaje);
+            return "/Doctores/Odontologos.xhtml";
+        }
+
+    }
+
+    public String citaEdita(String cedulaPaciente) {
+
+        Cita cita = CitaGestion.getCita(cedulaPaciente);
+
+        if (cita != null) {
+
+            this.setMotivoCita(cita.getMotivoCita());
+            this.setFechaCita(cita.getFechaCita());
+            this.setCedulaPaciente(cita.getCedulaPaciente());
+            return "/Pacientes/pacientes.xhtml";
+        } else {
+
+            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
+                    "Error", "Posiblemente el id no exista");
+            FacesContext.getCurrentInstance().addMessage("pacientesForm", mensaje);
+            return "/Pacientes/pacientes.xhtml";
+        }
+
     }
 }
