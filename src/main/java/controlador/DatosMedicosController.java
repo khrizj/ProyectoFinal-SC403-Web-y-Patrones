95f8d0db37a7360817b2d8de3b49fa3f95ac99d2
khrizj <khrizj@gmail.com>
2021-12-17 18:45:42 -0600

    Cita sql queries update

--- src/main/java/controlador/DatosMedicosController.java
+++ src/main/java/controlador/DatosMedicosController.java
@@ -3,12 +3,6 @@ package controlador;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
@@ -24,31 +18,30 @@ import gestion.DatosMedicosGestion;
 public class DatosMedicosController extends DatosMedicos implements Serializable {
 
     public DatosMedicosController() {
-        super("","","",false,"","","");
     }
     
      public String datosMedInserta() {
 
         if (DatosMedicosGestion.datosInsert(this)) {
-            return "/index.xhtml";
+            return "Odontologos.xhtml";
         } else {
             FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
                     "Error", "Posible Identificación Duplicada");
             FacesContext.getCurrentInstance().addMessage("nuevoPacienteForm:Cedula", mensaje);
-            return "/Pacientes/nuevoIngreso.xhtml";
+            return "Odontologos.xhtml";
         }
     }
 
     public String datosMedModifica() {
 
         if (DatosMedicosGestion.datosUpdate(this)) {
-            return "/Pacientes/pacientes.xhtml";
+            return "Odontologos.xhtml";
 
         } else {
             FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
                     "Error", "Posible Identificación Duplicada");
             FacesContext.getCurrentInstance().addMessage("editaPacienteForm:identificacion", mensaje);
-            return "/Pacientes/pacientes.xhtml";
+            return "Odontologos.xhtml";
         }
 
     }
@@ -56,38 +49,13 @@ public class DatosMedicosController extends DatosMedicos implements Serializable
     public String datosMedElimina() {
 
         if (DatosMedicosGestion.datosDelete(this)) {
-            return "/Doctores/Odontologos.xhtml";
+            return "Odontologos.xhtml";
         } else {
             FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
                     "Error", "Posible que el id no exista");
             FacesContext.getCurrentInstance().addMessage("editaPacienteForm:identificacion", mensaje);
-            return "/Doctores/Odontologos.xhtml";
+            return "Odontologos.xhtml";
         }
 
     }
-
-    public String datosMedEdita(String cedPaciente) {
-
-        DatosMedicos datosMed = DatosMedicosGestion.getDato(cedPaciente);
-
-        if (datosMed != null) {
-
-            this.setAntecedentesPatologicosPersonales(datosMed.getAntecedentesPatologicosPersonales());
-            this.setAntecedentesPatologicosFamiliares(datosMed.getAntecedentesPatologicosFamiliares());
-            this.setAntecedentesQuirurgicos(datosMed.getAntecedentesQuirurgicos());
-            this.setAlergia(datosMed.isAlergia());
-            this.setAlergiaDetalle(datosMed.getAlergiaDetalle());
-            this.setMedicamentosConsumidos(datosMed.getMedicamentosConsumidos());
-            this.setCedulaPaciente(datosMed.getCedulaPaciente());
-            return "/Pacientes/pacientes.xhtml";
-        } else {
-
-            FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
-                    "Error", "Posiblemente el id no exista");
-            FacesContext.getCurrentInstance().addMessage("pacientesForm", mensaje);
-            return "/Pacientes/pacientes.xhtml";
-        }
-
-    }
-
 }
--- src/main/java/controlador/EvolucionController.java
+++ src/main/java/controlador/EvolucionController.java
@@ -6,14 +6,9 @@ import gestion.EvolucionGestion;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
-import java.lang.reflect.Field;
-
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
-import java.util.List;
-import java.util.Objects;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 import modelo.Evolucion;
 
@@ -28,12 +23,12 @@ public class EvolucionController extends Evolucion implements Serializable {
     public String evolucionInserta() {
 
         if (EvolucionGestion.evolucionInsert(this)) {
-            return "Odontologos.xthml";
+            return "Odontologos.xhtml";
         } else {
             FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
                     "Error", "Posible Identificación Duplicada");
             FacesContext.getCurrentInstance().addMessage("evolucionForm:identificacion", mensaje);
-            return "Odontologos.xthml";
+            return "Odontologos.xhtml";
         }
     }
    
@@ -54,12 +49,12 @@ public class EvolucionController extends Evolucion implements Serializable {
     public String evolucionElimina() {
 
         if (EvolucionGestion.evolucionDelete(this)) {
-            return "/Doctores/Odontologos.xhtml";
+            return "Odontologos.xhtml";
         } else {
             FacesMessage mensaje = new FacesMessage(FacesMessage.SEVERITY_ERROR,
                     "Error", "Posible que el id no exista");
             FacesContext.getCurrentInstance().addMessage("evolucionForm:identificacion", mensaje);
-            return "/Doctores/Odontologos.xhtml";
+            return "Odontologos.xhtml";
         }
 
     }
